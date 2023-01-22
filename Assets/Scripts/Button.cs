c9088bb203cb3512639eefa09131e71b0e73d580
Kevin DELCROIX <k.delcroix@rubika-edu.com>
2023-01-22 18:07:22 +0100

    Added A Player Heigh Ref & Button Base

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+        // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.log("aaaaaaaaaaaah");
+        }
+    }
+}
